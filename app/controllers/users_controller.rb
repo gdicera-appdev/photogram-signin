1c4b80c39bfea99a4d69bafe8326b5266efb9391
gdicera <giuseppedicera@uchicago.edu>
2023-02-19 21:33:25 +0000

    finished sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,17 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
 
-    redirect_to("/users/#{user.username}")
+    save_status = user.save
+
+    if save_status == true
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence})
+    end
   end
 
   def update
